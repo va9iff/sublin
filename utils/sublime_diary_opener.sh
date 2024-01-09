6ef22c27991387b09a0738b89b0a4dfa3d767399
va9iff <va9iff@gmail.com>
2024-01-09 22:14:52 +0400

    refactor; change opening folders

--- utils/sublime_diary_opener.sh
+++ utils/sublime_diary_opener.sh
@@ -3,6 +3,7 @@ top=$top_at/diary
 
 year=$(date +%Y)
 month=$(date +%B)
+month_short=$(date +%b)
 day=$(date +%d)
 
 # everyday shortcut and launching opens the same day's page (a plain text file)
@@ -14,12 +15,12 @@ todays_page_path="$pages_month_folder/$month $day.txt"
 notes_folder=$top/notes
 
 # folder for every day. can be used to try stuff
-dumps_folder="$top/dumps/$year/$month"
-todays_dump="$dumps_folder/$month $day dump"
+dumps_folder="$top/dump/$year/$month"
+todays_dump="$dumps_folder/$month_short$day"
 
 mkdir -p "$pages_month_folder"
-mkdir -p "$dumps_folder"
+mkdir -p "$todays_dump"
 
-subl "$todays_page_path" "$todays_dump" "$notes_folder" "$pages_year_folder"
+subl "$todays_page_path" "$todays_dump" "$notes_folder" "$pages_year_folder" "$top/dump"
 
 # subl "~/diary/notes" "$(date +~/diary/%Y/%B)" "$(date +~/diary/%Y/%B/%B\ %d.txt)" "$(date +~/diary/%Y)"
