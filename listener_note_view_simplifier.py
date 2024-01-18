82cae100e37e7b562b65a5285bd37db00a0da21c
va9iff <va9iff@gmail.com>
2024-01-18 16:41:10 +0400

    prevent empty array erro

--- listener_note_view_simplifier.py
+++ listener_note_view_simplifier.py
@@ -5,10 +5,10 @@ import subprocess
 class RunCommandOnNewWindow(sublime_plugin.EventListener):
     def on_post_window_command(self, window, command_name, args):
         folders = window.folders()
-        if folders[0].split('/')[-1] == "notes":
+        if folders and folders[0].split('/')[-1] == "notes":
                 view = window.active_view()
                 # window.set_sidebar_visible(False)
-                window.set_tabs_visible(False)
+                # window.set_tabs_visible(False)
                 window.set_status_bar_visible(False)
                 window.set_minimap_visible(False)
                 view.settings().set('gutter',False)
