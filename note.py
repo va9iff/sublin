50b633c1d32fa8a5059c64da51d1205b49b0dc6d
va9iff <va9iff@gmail.com>
2024-01-18 18:29:55 +0400

    remove note.py as it can be done with a shell script anyway

--- note.py
+++
@@ -1,8 +0,0 @@
-import sublime
-import sublime_plugin
-import os
-from datetime import datetime
-
-class NoteCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        view = self.view.window().open_file(os.path.expanduser("~./note.txt"))
